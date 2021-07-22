e54e9d98df9577bc092c6076aa44a903161cd1d6
Ecris ton nom ici, en gardant les quotes <Ecris ton email ici, en gardant les quotes>
2021-07-22 18:25:27 +0200

    V1 OK - reste reférences, symbols, compteur victoires

--- app.rb
+++ app.rb
@@ -6,25 +6,62 @@ require_relative 'lib/app/player'
 require_relative 'lib/app/board'
 
 
-new_game = Positions.new
-new_board = Board.new(new_game)
+
+
+bienvenue_message = File.read("bienvenue.txt")
+print bienvenue_message
 puts "Player 1, quel est ton prénom ? \n"
 player_1 = Player.new(gets.chomp)
 puts "Player 2, quel est ton prénom ? \n"
 player_2 = Player.new(gets.chomp)
+player_wants_to_play = true
 
-puts new_game.table_has_winner? == ""
-puts !new_game.table_is_full?
-
-while new_game.table_has_winner? == "" && !new_game.table_is_full?
-  Player.all.each do |player|
-    if  new_game.table_has_winner? == ""
-        system "clear"
-        new_board.print_board
-        player.move(new_game)
+while player_wants_to_play == true
+  new_game = Positions.new
+  new_board = Board.new(new_game)
+  
+  while (new_game.table_has_winner? == "") && !new_game.table_is_full?
+    Player.all.each do |player|
+      if  new_game.table_has_winner? == "" && !new_game.table_is_full?
+          system "clear"
+          new_board.print_board
+          player.move(new_game)
+      end
     end
   end
+
+  system("clear")
+  if new_game.table_has_winner? != " "
+    puts "Le gagnant est"
+    puts "*-" *20+"*"
+    puts "*"+" "*39+"*"
+    puts "*"+"#{new_game.table_has_winner?}".center(39)+"*"
+    puts "*"+" "*39+"*"
+    puts "*"+"Tu as déglingué ton adversaire !".center(39)+"*"
+    puts "*"+" "*39+"*"
+    puts "*"+"Bravo à toi !".center(39)+"*"
+    puts "*"+" "*39+"*"
+    puts "*-" *20+"*"
+  else 
+    puts "Le gagnant est"
+    puts "*-" *20+"*"
+    puts "*"+" "*39+"*"
+    puts "*"+"What a close game! Match nul !!!".center(39)+"*"
+    puts "*"+" "*39+"*"
+    puts "*-" *20+"*"
+  end
+
+  puts
+  puts
+
+  print "Souhaitez-vous recommencer ? (y / toute autre touche)\n >>"
+
+
+  restart = gets.chomp
+  puts restart
+  restart == "y" ? player_wants_to_play = true : player_wants_to_play = false
+
+
 end
 
-puts "Le gagnant est #{new_game.table_has_winner?}"
-
+puts "A bientôt !"
--- lib/app/positions.rb
+++ lib/app/positions.rb
@@ -18,16 +18,10 @@ class Positions
   def table_has_winner?
     tab_victory = [diag_0_completed?, diag_1_completed?, line_0_completed?, line_1_completed?, line_2_completed?, vertical_0_completed?, vertical_1_completed?, vertical_2_completed?]
     # binding.pry
+    # print tab_victory
+    # puts
+    # sleep(3)
     return tab_victory.join.delete(" ")
-    # winner = nil
-    # tab_victory.each do |test_result|
-    #   if test_result != nil
-    #     winner = test_result
-    #   else
-    #     break
-    #   end
-    # end
-
   end
 
   def check_choice?(line,col)
@@ -36,16 +30,6 @@ class Positions
 
   def in_board?(line,col)
     return line >= 0 && line <=2 && col >= 0 && col <= 2
-  end
-
-  def print_board
-    print @moves[0]
-    puts
-    print @moves[1]
-    puts
-    print @moves[2]
-    puts
-    puts
   end
 
   def table_is_full?
@@ -61,35 +45,43 @@ class Positions
   end
 
   def diag_0_completed?
-    return @moves[0][0] if @moves[0][0] == @moves[1][1] && @moves[0][0] == @moves[2][2]
+    (@moves[0][0] == @moves[1][1] && @moves[0][0] == @moves[2][2]) ? @moves[0][0] : " "
+    # return @moves[0][0] if @moves[0][0] == @moves[1][1] && @moves[0][0] == @moves[2][2]
   end
 
   def diag_1_completed?
-    return @moves[0][2] if @moves[0][2] == @moves[1][1] && @moves[0][2] == @moves[2][0]
+    (@moves[0][2] == @moves[1][1] && @moves[0][2] == @moves[2][0]) ? @moves[0][2] : " "
+    # return @moves[0][2] if @moves[0][2] == @moves[1][1] && @moves[0][2] == @moves[2][0]
   end
 
   def line_0_completed?
-    return @moves[0][0] if @moves[0][0] == @moves[0][1] && @moves[0][0] == @moves[0][2]
+    (@moves[0][0] == @moves[0][1] && @moves[0][0] == @moves[0][2]) ? @moves[0][0] : " "
+    # return @moves[0][0] if @moves[0][0] == @moves[0][1] && @moves[0][0] == @moves[0][2]
   end
 
   def line_1_completed?
-    return @moves[1][0] if @moves[1][0] == @moves[1][1] && @moves[0][0] == @moves[1][2]
+    (@moves[1][0] == @moves[1][1] && @moves[1][0] == @moves[1][2]) ? @moves[1][0] : " "
+    # return @moves[1][0] if @moves[1][0] == @moves[1][1] && @moves[0][0] == @moves[1][2]
   end
 
   def line_2_completed?
-    return @moves[2][0] if @moves[2][0] == @moves[2][1] && @moves[0][0] == @moves[2][2]
+    (@moves[2][0] == @moves[2][1] && @moves[2][0] == @moves[2][2]) ? @moves[2][0] : " "
+    # return @moves[2][0] if @moves[2][0] == @moves[2][1] && @moves[0][0] == @moves[2][2]
   end
   
   def vertical_0_completed?
-    return @moves[0][0] if @moves[0][0] == @moves[1][0] && @moves[0][0] == @moves[2][0]
+    (@moves[0][0] == @moves[1][0] && @moves[0][0] == @moves[2][0]) ? @moves[0][0] : " "
+  #  f @moves[0][0] == @moves[1][0] && @moves[0][0] == @moves[2][0]
   end
   
   def vertical_1_completed?
-    return @moves[0][1] if @moves[0][1] == @moves[1][1] && @moves[0][1] == @moves[2][1]
+    (@moves[0][1] == @moves[1][1] && @moves[0][1] == @moves[2][1]) ? @moves[0][1] : " "
+    # return @moves[0][1] if @moves[0][1] == @moves[1][1] && @moves[0][1] == @moves[2][1]
   end
 
   def vertical_2_completed?
-    return @moves[0][2] if @moves[0][2] == @moves[1][2] && @moves[0][2] == @moves[2][2]
+    (@moves[0][2] == @moves[1][2] && @moves[0][2] == @moves[2][2]) ? @moves[0][2] : " "
+    # return @moves[0][2] if @moves[0][2] == @moves[1][2] && @moves[0][2] == @moves[2][2]
   end
 
 end
