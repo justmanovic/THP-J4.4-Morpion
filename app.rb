b40a80a9a24a0940fe13d31355fd6702ae6590e6
Ecris ton nom ici, en gardant les quotes <Ecris ton email ici, en gardant les quotes>
2021-07-22 09:22:10 +0200

    complete file - initial commit

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'bundler'
+Bundler.require
+
+require_relative 'lib/app/NOM_FICHIER_MAIN_CLASS_1_RB'
+require_relative 'lib/app/NOM_FICHIER_MAIN_CLASS_2_RB'
+require_relative 'lib/app/NOM_FICHIER_MAIN_CLASS_ETC_RB'
+require_relative 'lib/views/NOM_FICHIER_CLASSE_1_RB'
+require_relative 'lib/views/NOM_FICHIER_CLASSE_2_RB'
+require_relative 'lib/views/NOM_FICHIER_CLASSE_ETC_RB'
+
+#tu peux commencer à écrire la première méthode de ton programme
+def first_method
+end
+
+#décris ton programme
+def perform(var)
+end
+
+perfom(var)
